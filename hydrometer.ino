37f529948bf2e9ef9d12150a51d825894ca73cb9
brianhackel <brianhackel@mac.com>
2023-06-04 11:18:12 -0700

    adding wifi config

--- hydrometer.ino
+++ hydrometer.ino
@@ -2,67 +2,276 @@
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
 #include <math.h>
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESPAsyncWebSrv.h>
+#include <ESPAsyncTCP.h>
+#include "LittleFS.h"
+#include <ESP8266mDNS.h>
 
 Adafruit_MPU6050 mpu;
 
 float startingAngle;
 
-void setup() {
-  Serial.begin(115200);
-  while (!Serial)
-    delay(10);
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
 
-  Serial.println("Adafruit MPU6050 test!");
+// Search for parameter in HTTP POST request
+const char* PARAM_INPUT_1 = "ssid";
+const char* PARAM_INPUT_2 = "pass";
 
-  if (!mpu.begin()) {
-    Serial.println("Failed to find MPU6050 chip");
-    while (1) {
-      delay(10);
-    }
+//Variables to save values from HTML form
+String ssid;
+String pass;
+
+IPAddress local_ip(192,168,1,1);
+IPAddress gateway(192,168,1,1);
+IPAddress subnet(255,255,255,0);
+String hostname = "hydrometer";
+
+// File paths to save input values permanently
+const char* ssidPath = "/ssid.txt";
+const char* passPath = "/pass.txt";
+
+// Timer variables
+unsigned long previousMillis = 0;
+const long interval = 10000;  // interval to wait for Wi-Fi connection (milliseconds)
+
+// Set LED GPIO
+const int ledPin = 2;
+// Stores LED state
+
+String ledState;
+
+boolean restart = false;
+
+// Initialize LittleFS
+void initFS() {
+  if (!LittleFS.begin()) {
+    Serial.println("An error has occurred while mounting LittleFS");
+  }
+  else{
+    Serial.println("LittleFS mounted successfully");
+  }
+}
+
+
+// Read File from LittleFS
+String readFile(fs::FS &fs, const char * path){
+  Serial.printf("Reading file: %s\r\n", path);
+
+  File file = fs.open(path, "r");
+  if(!file || file.isDirectory()){
+    Serial.println("- failed to open file for reading");
+    return String();
   }
 
-  mpu.setAccelerometerRange(MPU6050_RANGE_2_G);
-  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
-  mpu.setFilterBandwidth(MPU6050_BAND_5_HZ);
+  String fileContent;
+  while(file.available()){
+    fileContent = file.readStringUntil('\n');
+    break;
+  }
+  file.close();
+  return fileContent;
+}
 
-  sensors_event_t startingOrientation, startG, startTemp;
-  mpu.getEvent(&startingOrientation, &startG, &startTemp);  // Store the starting orientation
+// Write file to LittleFS
+void writeFile(fs::FS &fs, const char * path, const char * message){
+  Serial.printf("Writing file: %s\r\n", path);
 
-  // TODO: need a function for getting the readings through averaging
+  File file = fs.open(path, "w");
+  if(!file){
+    Serial.println("- failed to open file for writing");
+    return;
+  }
+  if(file.print(message)){
+    Serial.println("- file written");
+  } else {
+    Serial.println("- frite failed");
+  }
+  file.close();
+}
 
-  startingAngle = getDisplacementAngle(startingOrientation.acceleration.x, startingOrientation.acceleration.y, startingOrientation.acceleration.z);
-  Serial.print("starting Angle: ");
-  Serial.print(startingAngle);
-  Serial.println(" degrees");
+// Initialize WiFi
+bool initWiFi() {
+  if(ssid==""){
+    Serial.println("Undefined SSID.");
+    return false;
+  }
 
-  delay(5000);
+  WiFi.mode(WIFI_STA);
+
+  WiFi.hostname(hostname);
+
+  WiFi.begin(ssid.c_str(), pass.c_str());
+
+  Serial.println("Connecting to WiFi...");
+  delay(20000);
+  if(WiFi.status() != WL_CONNECTED) {
+    Serial.println("Failed to connect.");
+    return false;
+  }
+
+  Serial.println(WiFi.localIP());
+
+  if (!MDNS.begin(hostname)) {
+    Serial.println("Error setting up MDNS responder!");
+  }
+  Serial.println("mDNS responder started");
+  return true;
+}
+
+// Replaces placeholder with LED state value
+String processor(const String& var) {
+  if(var == "STATE") {
+    if(!digitalRead(ledPin)) {
+      ledState = "ON";
+    }
+    else {
+      ledState = "OFF";
+    }
+    return ledState;
+  }
+  return String();
+}
+
+void setup() {
+  // Serial port for debugging purposes
+  Serial.begin(115200);
+
+  initFS();
+
+  // Set GPIO 2 as an OUTPUT
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
+  
+  // Load values saved in LittleFS
+  ssid = readFile(LittleFS, ssidPath);
+  pass = readFile(LittleFS, passPath);
+  Serial.println(ssid);
+  Serial.println(pass);
+
+  if(initWiFi()) {
+
+    if (!mpu.begin()) {
+      Serial.println("Failed to find MPU6050 chip");
+      while (1) {
+        delay(10);
+      }
+    }
+
+    mpu.setAccelerometerRange(MPU6050_RANGE_2_G);
+    mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+    mpu.setFilterBandwidth(MPU6050_BAND_5_HZ);
+
+    sensors_event_t startingOrientation, startG, startTemp;
+    mpu.getEvent(&startingOrientation, &startG, &startTemp);  // Store the starting orientation
+
+    // TODO: need a function for getting the readings through averaging
+
+    startingAngle = getDisplacementAngle(startingOrientation.acceleration.x, startingOrientation.acceleration.y, startingOrientation.acceleration.z);
+    Serial.print("starting Angle: ");
+    Serial.print(startingAngle);
+    Serial.println(" degrees");
+
+
+    // Route for root / web page
+    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+      request->send(LittleFS, "/index.html", "text/html", false, processor);
+    });
+    
+    server.serveStatic("/", LittleFS, "/");
+    
+    // Route to set GPIO state to HIGH
+    server.on("/on", HTTP_GET, [](AsyncWebServerRequest *request) {
+      digitalWrite(ledPin, LOW);
+      request->send(LittleFS, "/index.html", "text/html", false, processor);
+    });
+
+    // Route to set GPIO state to LOW
+    server.on("/off", HTTP_GET, [](AsyncWebServerRequest *request) {
+      digitalWrite(ledPin, HIGH);
+      request->send(LittleFS, "/index.html", "text/html", false, processor);
+    });
+    server.begin();
+  }
+  else {
+    // Connect to Wi-Fi network with SSID and password
+    Serial.println("Setting AP (Access Point)");
+    // NULL sets an open Access Point
+    WiFi.softAP("ESP-WIFI-MANAGER", NULL);
+
+    Serial.print("Setting soft-AP configuration ... ");
+    Serial.println(WiFi.softAPConfig(local_ip, gateway, subnet) ? "Ready" : "Failed!");
+
+    IPAddress IP = WiFi.softAPIP();
+    Serial.print("AP IP address: ");
+    Serial.println(IP); 
+
+    // Web Server Root URL
+    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+      request->send(LittleFS, "/wifimanager.html", "text/html");
+    });
+    
+    server.serveStatic("/", LittleFS, "/");
+    
+    server.on("/", HTTP_POST, [](AsyncWebServerRequest *request) {
+      int params = request->params();
+      for(int i=0;i<params;i++){
+        AsyncWebParameter* p = request->getParam(i);
+        if(p->isPost()){
+          // HTTP POST ssid value
+          if (p->name() == PARAM_INPUT_1) {
+            ssid = p->value().c_str();
+            Serial.print("SSID set to: ");
+            Serial.println(ssid);
+            // Write file to save value
+            writeFile(LittleFS, ssidPath, ssid.c_str());
+          }
+          // HTTP POST pass value
+          if (p->name() == PARAM_INPUT_2) {
+            pass = p->value().c_str();
+            Serial.print("Password set to: ");
+            Serial.println(pass);
+            // Write file to save value
+            writeFile(LittleFS, passPath, pass.c_str());
+          }
+
+          //Serial.printf("POST[%s]: %s\n", p->name().c_str(), p->value().c_str());
+        }
+      }
+      restart = true;
+      request->send(200, "text/plain", "Done. ESP will restart, connect to your router and go to http://" + hostname + ".local");
+    });
+    server.begin();
+  }
 }
 
 void loop() {
-  sensors_event_t a, g, temp;
-  mpu.getEvent(&a, &g, &temp);
+  if (restart){
+    delay(5000);
+    ESP.restart();
+  } else {
+    MDNS.update();
+    /*sensors_event_t a, g, temp;
+    mpu.getEvent(&a, &g, &temp);
 
-  Serial.print("Acceleration X: ");
-  Serial.print(a.acceleration.x);
-  Serial.print(", Y: ");
-  Serial.print(a.acceleration.y);
-  Serial.print(", Z: ");
-  Serial.println(a.acceleration.z);
+    Serial.print("Acceleration X: ");
+    Serial.print(a.acceleration.x);
+    Serial.print(", Y: ");
+    Serial.print(a.acceleration.y);
+    Serial.print(", Z: ");
+    Serial.println(a.acceleration.z);
 
-  float angle = startingAngle - getDisplacementAngle(a.acceleration.x, a.acceleration.y, a.acceleration.z);
-  Serial.print("Displacement Angle: ");
-  Serial.print(angle);
-  Serial.println(" degrees");
+    float angle = startingAngle - getDisplacementAngle(a.acceleration.x, a.acceleration.y, a.acceleration.z);
+    Serial.print("Displacement Angle: ");
+    Serial.print(angle);
+    Serial.println(" degrees");
 
-  delay(2000);
+    delay(2000);*/
+  }
 }
 
 float getDisplacementAngle(float accelX, float accelY, float accelZ) {
-  // float angleX = atan2(accelY, sqrt(accelX * accelX + accelZ * accelZ));
-  // float angleY = atan2(-accelX, sqrt(accelY * accelY + accelZ * accelZ));
-
-  // float angle = sqrt(angleX * angleX + angleY * angleY);
-  // return angle * (180.0 / M_PI);
-
   return acos(abs(accelY) / (sqrt(accelX * accelX + accelY * accelY + accelZ * accelZ))) * 180.0 / M_PI;
 }
