032f4b8edf41a20a48c4fc712aba93de3f882b53
brianhackel <brianhackel@mac.com>
2023-08-25 11:05:17 -0700

    covert battery counts to percentage

--- Battery.cpp
+++ Battery.cpp
@@ -7,9 +7,10 @@ long Battery::getPercentage() {
   // lipo value of 4.2V and drops it to 0.758V max.
   // this means our min analog read value should be 580 (3.14V)
   // and the max analog read value should be 774 (4.2V).
+  // BMH: empirically, we will use different values (see header for definitions)
   int level = analogRead(A0);
 
   // convert battery level to percent
-//  level = map(level, 580, 774, 0, 100);
+  level = map(level, BATTERY_COUNTS_LOW, BATTERY_COUNTS_HIGH, 0, 100);
   return level;
 }
--- Battery.h
+++ Battery.h
@@ -2,6 +2,9 @@
 #define _BATTERY_H
 #include <Arduino.h>
 
+#define BATTERY_COUNTS_HIGH 783
+#define BATTERY_COUNTS_LOW 580
+
 class Battery {
   public:
    static long getPercentage();
