630f453ea992ffd0ed9dcb3338bfacca63b049d1
brianhackel <brianhackel@mac.com>
2023-08-29 00:09:00 -0700

    change double reset timeout

--- hydrometer.ino
+++ hydrometer.ino
@@ -18,7 +18,7 @@
 
 // Number of seconds after reset during which a 
 // subseqent reset will be considered a double reset.
-#define DRD_TIMEOUT 10
+#define DRD_TIMEOUT 1
 
 // RTC Memory Address for the DoubleResetDetector to use
 #define DRD_ADDRESS 0
