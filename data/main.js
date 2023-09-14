cea5b4a59a312117c0dde4b6e94e44b7531e5384
brianhackel <brianhackel@mac.com>
2023-09-13 20:07:14 -0700

    chart panes

--- data/main.js
+++ data/main.js
@@ -5,141 +5,171 @@ document.addEventListener('DOMContentLoaded', function () {
         },
 
         title: null,
-
-        pane: {
-            center: ['50%', '85%'],
-            size: '120%',
-            startAngle: -90,
-            endAngle: 90,
-            background: {
-                backgroundColor:
-                    Highcharts.defaultOptions.legend.backgroundColor || '#EEE',
-                innerRadius: '60%',
-                outerRadius: '100%',
-                shape: 'arc'
-            }
-        },
+// TODO: remove duplicate code by cascading options that are shared
+        pane: [{
+          center: ['16.66%', '85%'],
+          size: '140%',
+          startAngle: -90,
+          endAngle: 90,
+          background: {
+              backgroundColor:
+                  Highcharts.defaultOptions.legend.backgroundColor || '#EEE',
+              innerRadius: '60%',
+              outerRadius: '100%',
+              shape: 'arc'
+          }
+        }, {
+          center: ['50%', '85%'],
+          size: '140%',
+          startAngle: -90,
+          endAngle: 90,
+          background: {
+              backgroundColor:
+                  Highcharts.defaultOptions.legend.backgroundColor || '#EEE',
+              innerRadius: '60%',
+              outerRadius: '100%',
+              shape: 'arc'
+          }
+        }, {
+          center: ['83.26%', '85%'],
+          size: '140%',
+          startAngle: -90,
+          endAngle: 90,
+          background: {
+            backgroundColor:
+                Highcharts.defaultOptions.legend.backgroundColor || '#EEE',
+            innerRadius: '60%',
+            outerRadius: '100%',
+            shape: 'arc'
+          }
+        }],
 
         credits: {
             enabled: false
         },
 
         // the value axis
-        yAxis: {
-            lineWidth: 0,
-            tickWidth: 0,
-            minorTickInterval: null,
-            tickAmount: 2,
-            title: {
-                y: -45,
-                style: {
-                    fontSize: "1.2em"
-                }
-            },
-            labels: {
-                y: 16
+        yAxis: [{
+          pane: 0,
+          lineWidth: 0,
+          tickWidth: 0,
+          minorTickInterval: null,
+          tickAmount: 2,
+          title: {
+            text: 'Tilt',
+            y: 63,
+            style: {
+              fontSize: "1.3em"
             }
-        },
-
-        plotOptions: {
-            solidgauge: {
-                dataLabels: {
-                    style: {
-                        fontSize: '0.8em'
-                    },
-                    y: -25,
-                    borderWidth: 0,
-                    useHTML: true
-                }
-            }
-        }
-    };
-
-    // The tilt gauge
-    const chartTilt = Highcharts.chart('chart-tilt', Highcharts.merge(gaugeOptions, {
-        yAxis: {
-            min: 0,
-            max: 90,
-            tickPositions: [0, 90],
-            title: {
-                text: 'Tilt'
-            },
-            stops: [
+          },
+          labels: {
+            y: 16
+          },
+          min: 0,
+          max: 90,
+          tickPositions: [0, 90],
+          stops: [
             [0.1, '#ffff00'], // yellow
             [0.5, '#ff8c00'], // orange
             [0.9, '#321414'] // brown
-            ],
-        },
-
-        series: [{
-            name: 'Tilt',
-            data: [0],
-            dataLabels: {
-                format:
-                    '{y:.1f}&deg;'
-            },
-            tooltip: {
-                valueSuffix: ' &deg;'
+          ]
+        } , {
+          pane: 1,
+          lineWidth: 0,
+          tickWidth: 0,
+          minorTickInterval: null,
+          tickAmount: 2,
+          title: {
+            text: 'Temperature',
+            y: 63,
+            style: {
+              fontSize: "1.3em"
             }
-        }]
-    }));
-
-    // The Temperature gauge
-    const chartTemperature = Highcharts.chart('chart-temperature', Highcharts.merge(gaugeOptions, {
-        yAxis: {
-            min: 30,
-            max: 90,
-            title: {
-                text: 'Temperature'
-            },
-            stops: [
+          },
+          labels: {
+            y: 16
+          },
+          min: 30,
+          max: 90,
+          stops: [
             [0.1, '#0000ff'], // blue
             [0.5, '#ffff00'], // yellow
             [0.9, '#ff0000'] // red
-            ],
-        },
-
-        series: [{
-            name: 'Temperature',
-            data: [30],
-            dataLabels: {
-                format:
-                    '{y:.1f}&deg;F'
-            },
-            tooltip: {
-                valueSuffix: ' &deg;F'
+          ]
+        } , {
+          pane: 2,
+          lineWidth: 0,
+          tickWidth: 0,
+          minorTickInterval: null,
+          tickAmount: 2,
+          title: {
+            text: 'Battery',
+            y: 63,
+            style: {
+              fontSize: "1.3em"
             }
-        }]
-    }));
-
-    // The Battery gauge
-    const chartBattery = Highcharts.chart('chart-battery', Highcharts.merge(gaugeOptions, {
-        yAxis: {
-            min: 0,
-            max: 100,
-            title: {
-                text: 'Battery'
-            },
-            stops: [
+          },
+          labels: {
+              y: 16
+          },
+          min: 0,
+          max: 100,
+          stops: [
             [0.1, '#ff0000'], // red
             [0.5, '#0000ff'], // blue
             [0.9, '#008000'] // green
-            ],
+          ]
+        }],
 
-        },
-
-        series: [{
-            name: 'Battery',
-            data: [100],
+        plotOptions: {
+          solidgauge: {
             dataLabels: {
-                format: '{y}%'
-            },
-            tooltip: {
-                valueSuffix: ' %'
+              style: {
+                fontSize: '0.8em'
+              },
+              y: -35,
+              borderWidth: 0,
+              useHTML: true
             }
+          }
+        }
+    };
+
+    const chart = Highcharts.chart('chart', Highcharts.merge(gaugeOptions, {
+        series: [{
+          name: 'Tilt',
+          data: [0],
+          dataLabels: {
+              format:
+                '{y:.1f}&deg;'
+          },
+          tooltip: {
+            valueSuffix: ' &deg;'
+          },
+          yAxis: 0
+        }, {
+          name: 'Temperature',
+          data: [30],
+          dataLabels: {
+              format:
+                '{y:.1f}&deg;F'
+          },
+          tooltip: {
+            valueSuffix: ' &deg;F'
+          },
+          yAxis: 1
+        }, {
+          name: 'Battery',
+          data: [0],
+          dataLabels: {
+            format: '{y}%'
+          },
+          tooltip: {
+            valueSuffix: ' %'
+          },
+          yAxis: 2
         }]
     }));
-
 
     $(document).ready(function(){
         $('.chart-container').hide();
@@ -154,17 +184,18 @@ document.addEventListener('DOMContentLoaded', function () {
           $('.chart-container').show();
     
           $.getJSON( "/state", function( data ) {
-            if (chartTilt && "angle" in data) {
-              chartTilt.series[0].points[0].update(parseFloat(data["angle"]));
+            if (chart && "angle" in data) {
+              chart.series[0].points[0].update(parseFloat(data["angle"]));
             }
-            if (chartTemperature && "temperature" in data) {
-              chartTemperature.series[0].points[0].update(parseFloat(data["temperature"]));
+            if (chart && "temperature" in data) {
+              chart.series[1].points[0].update(parseFloat(data["temperature"]));
             }
-            if (chartBattery && "battery" in data) {
-              chartBattery.series[0].points[0].update(parseInt(data["battery"]));
+            if (chart && "battery" in data) {
+              chart.series[2].points[0].update(parseInt(data["battery"]));
             }
           });
         }, 1000);
+
         $("#configuration input").each(function () {
           $(this).rules("add", {
             required: true
