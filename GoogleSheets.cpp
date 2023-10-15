ede029a166f2e1e0d1230588d79278210ff718ff
brianhackel <brianhackel@mac.com>
2023-10-15 12:59:46 -0700

    Update GoogleSheets.cpp

--- GoogleSheets.cpp
+++ GoogleSheets.cpp
@@ -3,18 +3,10 @@
 const String GoogleSheets::NAME = "GoogleSheets";
 const String GoogleSheets::BASE_URL = "https://script.google.com/macros/s/";
 
-GoogleSheets::GoogleSheets(const String& deploymentId, const String& spreadsheetId, const String& sheetName) : Poster(BASE_URL + deploymentId + "/exec", NAME), _spreadsheetId(spreadsheetId), _sheetName(sheetName) {};
-
-/*
-{"temp":"77",
-"tilt":"44",
-"battery":"88",
-"sheetName":"Log",
-"spreadsheetId":"1Vm_yyzYJGHxuL6VfqA0Vs1Op3U4c2ru-9uVMdvuiXVs"} 
-
-https://script.google.com/macros/s/AKfycbwaDRFxaTrB77QBs42C7wUi7NCoxxAsYd1n5g4Cpo6R-oWpYTpNfPOOwiyXs9RCNHgp/exec
-*/
-
+GoogleSheets::GoogleSheets(const String& deploymentId, const String& spreadsheetId, const String& sheetName) : 
+  Poster(BASE_URL + deploymentId + "/exec", NAME), 
+  _spreadsheetId(spreadsheetId), 
+  _sheetName(sheetName) {};
 
 String GoogleSheets::getJson(float angle, float temperature, long battery) const {
   String jsonString = "";
@@ -24,6 +16,10 @@ String GoogleSheets::getJson(float angle, float temperature, long battery) const
   jsonString += angle;
   jsonString += "\",\"battery\":\"";
   jsonString += battery;
+  jsonString += "\",\"spreadsheetId\":\"";
+  jsonString += _spreadsheetId;
+  jsonString += "\",\"sheetName\":\"";
+  jsonString += _sheetName;
   jsonString += "\"}";
   return jsonString;
 }
